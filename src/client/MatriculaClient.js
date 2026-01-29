481016fdd5e18fc4f539a31d8775f6c7b859a407
aaaaaa <aaaa@gmail.com>
2026-01-28 19:47:35 -0500

    vue

---
+++ src/client/MatriculaClient.js
@@ -0,0 +1,7 @@
+import axios from "axios"
+
+const consultarTodos = () => {
+    const data = axios.get('http://localhost:8081/matricula/api/v1.0/estudiantes').then(r=>r.data)
+    return data;
+ }
+ 
